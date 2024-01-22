b88d8655d68854aa34e2ae5e4948863bf7951d17
Hohm <Hohm@HOHM>
2024-01-22 14:33:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            string name = "Nelca";
+            int age = 25;
+            bool isTall = false;
+            double weight = 60.2;
+
+            Console.WriteLine(name + " is " + age  + " she weighs " + weight );
+            Console.WriteLine("it is "   +   isTall +   " that she is tall.");
+
+            //freeze console
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
